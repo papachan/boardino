d3e90c5c85acedfbe05d8c0e3e393bc9d796be5b
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2012-05-08 23:22:48 -0500

    Cambios para incluir pde del board en la vista

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^', include('whiteboard.board.urls')),
@@ -15,4 +17,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
