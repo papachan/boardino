14b43763a76dcce985e4390e5a71d6c92d1ba1bc
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2012-05-09 22:14:23 -0500

    Agregado modelo Post It

--- board/models.py
+++ board/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 class Board(models.Model):
+
     def __unicode__(self):
         return self.id
+
+class PostIt(models.Model):
+    board = models.ForeignKey(Board)
+    text = models.TextField()
--- board/views.py
+++ board/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
-from board.models import Board
+from board.models import Board, PostIt
 
 def create_board(request):
     board = Board()
+    postit = PostIt(text="Soy un postit")
+    board.postit_set.add(postit)
     board.save()
     return HttpResponseRedirect(board.id)
 
 def board(request, board_id):
-    the_board = get_object_or_404(Board, pk=board_id)
-    return render_to_response('board.html',{'board':the_board})
+    board = get_object_or_404(Board, pk=board_id)
+    return render_to_response('board.html',{'postits':board.postit_set.all()})
