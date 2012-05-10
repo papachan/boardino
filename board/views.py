ccd4da8a4693b13a05735c2bf694e296cec71066
Germán Mauricio Muñoz <gmauricio.munoz@gmail.com>
2012-05-09 21:27:57 -0500

    Creada persistencia de tablero

--- board/views.py
+++ board/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from board.models import Board
 
 def create_board(request):
-    id = 1
-    return HttpResponseRedirect("1")
+    board = Board()
+    board.save()
+    return HttpResponseRedirect(board.id)
 
 def board(request, board_id):
-    return render_to_response('board.html')
+    the_board = get_object_or_404(Board, pk=board_id)
+    return render_to_response('board.html',{'board':the_board})
