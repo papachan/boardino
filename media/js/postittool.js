1d44ecb01cc6b5862ef248f80b6558c9a5487982
mauricio <gmauricio.munoz@gmail.com>
2012-09-01 10:24:33 -0500

    refactoring: separada lógica de herramienta para postits

---
+++ media/js/postittool.js
@@ -0,0 +1,97 @@
+function PostitTool(postitToolListener){
+    this.postitToolListener = postitToolListener;
+}
+
+PostitTool.prototype.addPostits = function(postits) {
+    var _this = this;
+    $.each(postits, function(i, postit) {
+        _this.createPostit(postit.id, postit.text, postit.x, postit.y, postit.width, postit.height);
+    });
+};
+
+PostitTool.prototype.createPostit = function(id, text, x, y, width, height){
+    var postit = this.createPostitDiv(id, x, y, width, height).appendTo("#board");
+    var postitCloseElement = this.createPostitCloseElement(id).appendTo(postit);
+    var postitTextArea = this.createPostitTextArea(id).appendTo(postit);
+    postitTextArea.val(text);
+    postitTextArea.keyup(function(){
+        onUpdatePostit(id, x, y, postitTextArea.val());
+    });
+    setTimeout(function() { postitTextArea.focus(); }, 0);
+};
+
+PostitTool.prototype.createPostitTextArea = function(postitId){
+    return $("<textarea/>").addClass("postit_input");
+};
+
+PostitTool.prototype.createPostitCloseElement = function(postitId){
+    return $("<img/>")
+            .addClass("postit_close_image")
+            .attr("src", "/media/close.png")
+            .click(function(){
+                onDeletedPostit(postitId);
+            });
+};
+
+PostitTool.prototype.createPostitDiv = function(postitId, x, y, width, height){
+    var postitDivId = "postit"+postitId;
+    postitElement = $("<div/>")
+            .attr("id", postitDivId)
+            .addClass("postit")
+            .css("position", "absolute")
+            .css("top", y+"px")
+            .css("left", x+"px")
+            .css("width", width+"px")
+            .css("height", height+"px")
+            .css("padding", "2px 2px 25px 2px");
+
+    postitElement.draggable({
+                    containment: "parent",
+                    drag: function(){
+                        var position = $(this).position();
+                        boardConnection.movePostit(postitId, position.left, position.top);
+                    },
+                    stop: function(){
+                        var position = $(this).position();
+                        savePostitPosition(postitId, position.left, position.top);
+                    }
+            })
+            .resizable({
+                    resize: function(){
+                        var width = $(this).width();
+                        var height = $(this).height();
+                        boardConnection.resizePostit(postitId, width, height);
+                    },
+                    stop: function(event, ui){
+                        var width = ui.size.width;
+                        var height = ui.size.height;
+                        savePostitSize(postitId, width, height);
+                    }
+            });
+    return postitElement;
+};
+
+PostitTool.prototype.movePostit = function(id, newX, newY){
+    var postit = this.getPostit(id);
+    postit.css('top', newY+"px");
+    postit.css('left',newX+"px");
+};
+
+PostitTool.prototype.resizePostit = function(id, width, height){
+    var postit = this.getPostit(id);
+    postit.width(width);
+    postit.height(height);
+};
+
+PostitTool.prototype.updatePostitText = function(id, text){
+    this.getPostit(id).find("textarea").val(text);
+};
+
+PostitTool.prototype.deletePostit = function(id){
+    this.getPostit(id).find("textarea").remove();
+    this.getPostit(id).remove();
+};
+
+PostitTool.prototype.getPostit = function(id){
+    return $("#postit"+id);
+};
