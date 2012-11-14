7010238e55996af25b4cb5584240b3262b5f8b17
Billy Everyteen <mauricio@mapalesoftware.com>
2012-11-13 23:05:53 -0500

    added migration file for new Line field

---
+++ board/migrations/0007_auto__add_field_line_path.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Line.path'
+        db.add_column('board_line', 'path',
+                      self.gf('django.db.models.fields.TextField')(null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Line.path'
+        db.delete_column('board_line', 'path')
+
+
+    models = {
+        'board.board': {
+            'Meta': {'object_name': 'Board'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'password': ('django.db.models.fields.TextField', [], {'default': "''"})
+        },
+        'board.line': {
+            'Meta': {'object_name': 'Line'},
+            'board': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['board.Board']"}),
+            'color_l': ('django.db.models.fields.TextField', [], {'default': "'000000'"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'stroke_w': ('django.db.models.fields.IntegerField', [], {}),
+            'x': ('django.db.models.fields.IntegerField', [], {}),
+            'x1': ('django.db.models.fields.IntegerField', [], {}),
+            'y': ('django.db.models.fields.IntegerField', [], {}),
+            'y1': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'board.postit': {
+            'Meta': {'object_name': 'PostIt'},
+            'back_color': ('django.db.models.fields.TextField', [], {'default': "'#FFFF33'"}),
+            'board': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['board.Board']"}),
+            'color': ('django.db.models.fields.TextField', [], {'default': "'#FFFF99'"}),
+            'height': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'width': ('django.db.models.fields.IntegerField', [], {}),
+            'x': ('django.db.models.fields.IntegerField', [], {}),
+            'y': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['board']
