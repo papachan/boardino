46276e0c8c3fac5d91b9ab7c80bf86c2f3b48a5b
papachan <papachan@gmail.com>
2013-08-12 16:20:13 -0500

    changing database to sqlite

--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'whiteboard',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'password',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'whiteboard.db',   # Or path to database file if using sqlite3.
+        'USER': '',                # Not used with sqlite3.
+        'PASSWORD': '',            # Not used with sqlite3.
+        'HOST': '',                # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -68,7 +68,7 @@ COMPRESS_URL = STATIC_URL
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -83,18 +83,19 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder'
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '12nx7-&t@%$zb1v=968mcs0ow@!)u+62#sf4zggu%1_7pg^1g('
 
-SESSION_ENGINE= 'django.contrib.sessions.backends.signed_cookies'
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -120,19 +121,15 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_nose',
     'lettuce.django',
-    #'django_jasmine',
     'south',
     'board',
     'compressor',
     'rest_framework'
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 
-JASMINE_TEST_DIRECTORY = MEDIA_ROOT+'/js/jasmine'
+# JASMINE_TEST_DIRECTORY = MEDIA_ROOT+'/js/jasmine'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
