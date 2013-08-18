4b6f326d979f37e0351f9642081d65af770b84ed
Mauricio Muñoz <gmauricio.munoz@gmail.com>
2013-08-18 12:18:33 -0500

    deleted js code that has been moved to a separated project

--- static/js/collections/lines.js
+++
@@ -1,9 +0,0 @@
-define([
-  'models/line'
-], function(Line){
-    var LineList = Backbone.Collection.extend({
-        model: Line,
-        url: "api/boards/"+board_id+"/lines/"
-    });
-    return LineList;
-});
--- static/js/collections/postits.js
+++
@@ -1,9 +0,0 @@
-define([
-  'models/postit'
-], function(Postit){
-    var PostitList = Backbone.Collection.extend({
-        model: Postit,
-        url: "api/boards/"+board_id+"/postits/"
-    });
-    return PostitList;
-});
